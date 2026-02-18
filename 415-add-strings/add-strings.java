50c0c14b51a6248e9af98fd24e276c9388c36a2c
manishyadav73 <manishkryadav7319@gmail.com>
2026-02-18 23:54:14 +0530

    Time: 2 ms (98.20%) | Memory: 43.8 MB (78.87%) - LeetSync

---
+++ 415-add-strings/add-strings.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder result=new StringBuilder();
+        int i=num1.length()-1;
+        int j=num2.length()-1;
+        int carry=0;
+        while(i>=0||j>=0||carry>0){
+            int digit1;
+            if(i>=0){
+                digit1=num1.charAt(i)-'0';
+            }else{
+                digit1=0;
+            }
+            int digit2;
+            if(j>=0){
+                digit2=num2.charAt(j)-'0';
+            }else{
+                digit2=0;
+            }
+            int sum=digit1+digit2+carry;
+            carry=sum/10;
+            result.append(sum%10);
+            i--;
+            j--;
+        }
+         return result.reverse().toString();
+    }
+}
