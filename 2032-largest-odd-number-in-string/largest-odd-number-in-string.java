3628842c3ffd874104fbaf52ea8cd1230af4a7e4
Manish Kumar <manishkryadav7319@gmail.com>
2026-07-05 23:56:24 +0530

    Time: 15 ms (5.51%) | Memory: 47.2 MB (36.03%) - LeetSync

--- 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
+++ 2032-largest-odd-number-in-string/largest-odd-number-in-string.java
@@ -1,16 +1,32 @@
-class Solution {
-    public String largestOddNumber(String num) {
+// class Solution {
+//     public String largestOddNumber(String num) {
       
-        for (int i = num.length() - 1; i >= 0; i--) {
-            int digit = num.charAt(i) - '0';
+//         for (int i = num.length() - 1; i >= 0; i--) {
+//             int digit = num.charAt(i) - '0';
             
          
-            if (digit % 2 != 0) {
-                return num.substring(0, i + 1);
-            }
-        }
+//             if (digit % 2 != 0) {
+//                 return num.substring(0, i + 1);
+//             }
+//         }
         
        
+//         return "";
+//     }
+// }
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+class Solution {
+    public String largestOddNumber(String num) {
+        // Regex looks for the longest prefix that ends with an odd digit (1, 3, 5, 7, 9)
+        Pattern pattern = Pattern.compile("^.*[13579]");
+        Matcher matcher = pattern.matcher(num);
+        
+        if (matcher.find()) {
+            return matcher.group(); // Returns the matched substring
+        }
+        
         return "";
     }
 }
