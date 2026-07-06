0ce3615b9200623955c6e9f3251d48a21e8399f5
Manish Kumar <manishkryadav7319@gmail.com>
2026-07-06 23:33:53 +0530

    Time: 4 ms (82.37%) | Memory: 46.8 MB (9.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.java
+++ 242-valid-anagram/valid-anagram.java
@@ -1,21 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
-        // int[] count = new int[26];
-        // for (int i = 0; i < s.length(); i++) {
-        //     count[s.charAt(i) - 'a']++;
-        // }
-        // for (int i = 0; i < t.length(); i++) {
-        //     count[t.charAt(i) - 'a']--;
-        // }
-        // for (int num : count) {
-        //     if (num != 0) return false;
-        // } 
-        // return true;
-        char arr1[]=s.toCharArray();
-        char arr2[]=t.toCharArray();
-        Arrays.sort(arr1);
-        Arrays.sort(arr2);
-        return Arrays.equals(arr1,arr2);
+//         HashMap<Character,Integer>map=new HashMap<>();
+
+//         for(char ch:s.toCharArray()){
+//             map.put( ch,map.getOrDefault(ch,0)+1);
+//         }
+//         for(char ch:t.toCharArray()){
+//             map.put( ch,map.getOrDefault(ch,0)-1);
+//             if(map.get(ch)==0){
+//                 map.remove(ch);
+//             }
+//         }
+// return map.isEmpty();
+char a[]=s.toCharArray();
+char b[]=t.toCharArray();
+Arrays.sort(a);
+Arrays.sort(b);
+return Arrays.equals(a,b);
     }
 }
