95071d27f100148b70abf09ebff1847ca811e010
Manish Kumar <manishkryadav7319@gmail.com>
2026-07-12 00:18:54 +0530

    Time: 2 ms (29.96%) | Memory: 43.4 MB (20.48%) - LeetSync

--- 812-rotate-string/rotate-string.java
+++ 812-rotate-string/rotate-string.java
@@ -1,12 +1,12 @@
 class Solution {
     public boolean rotateString(String s, String goal) {
-        // Agar dono ki length alag hai, toh rotate karke bhi same nahi ho sakte
+        
         if (s.length() != goal.length()) {
             return false;
         }
-        
-        // s ko do baar jod kar check karein ki goal usme maujood hai ya nahi
-        String doubledString = s + s;
-        return doubledString.contains(goal);
+        String sum=s+s;
+        return sum.contains(goal);
+     
+     
     }
 }
