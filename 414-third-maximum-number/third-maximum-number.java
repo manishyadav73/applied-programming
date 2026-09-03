affe983aa4761ad81fdb5c7a37e5180e3d319fae
Manish Kumar <manishkryadav7319@gmail.com>
2026-09-03 18:48:14 +0530

    Time: 8 ms (14.40%) | Memory: 46.2 MB (16.66%) - LeetSync

---
+++ 414-third-maximum-number/third-maximum-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int thirdMax(int[] nums) {
+       
+        Set<Integer>set=new HashSet<>();
+        for(int n:nums){
+            if(!set.contains(n)){
+                set.add(n);
+            }
+        }
+        List<Integer>list=new ArrayList<>(set);
+        Collections.sort(list);
+        
+        int m=list.size();
+        if(m<3){
+            return list.get(m-1);
+        }
+        return list.get(m-3);
+    }
+}
